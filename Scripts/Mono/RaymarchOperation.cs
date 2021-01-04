23a1cd076b3ff3a6bda1226aea48cae662fca893
Brendan Lienau <380790@colum.edu>
2021-01-03 21:43:42 -0600

    Raymarch shape and operation classes used to dynamically alter shapes at both design time & run time

---
+++ Scripts/Mono/RaymarchOperation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class Operation : MonoBehaviour
+{
+    public enum OpFunction
+    {
+        None,
+        Subtract,
+        Intersect,
+        Blend
+    }
+
+    public OpFunction operation;
+    public int childCount;
+
+    public float blendStrength;
+
+    public bool lerpBlend;
+
+    public float minBlend, maxBlend, lerpSpeed;
+
+    void Update()
+    {
+        if (operation == OpFunction.Blend)
+            BlendUpdate();
+    }
+
+    void BlendUpdate()
+    {
+        if (minBlend < 0)
+            minBlend = 0;
+        if (maxBlend < minBlend)
+            maxBlend = minBlend;
+        if (lerpBlend)
+            blendStrength = Mathf.Lerp(minBlend, maxBlend, Mathf.Abs(Mathf.Sin(Time.time * lerpSpeed)));
+    }
+}
+
+public struct OperationInfo
+{
+    public int operation;
+    public int childCount;
+
+    public float blendStrength;
+
+    public static int GetSize()
+    {
+        return sizeof(int) * 2 + sizeof(float) * 1;
+    }
+}
---
+++ Scripts/Mono/RaymarchShape.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class RaymarchShape : MonoBehaviour
+{
+    public ShapeInfo shapeInfo;
+    public enum Shape
+    {
+        Sphere,
+        Box,
+        Torus,
+        Cone,
+        RoundedBox
+    }
+
+
+    /// <summary>
+    /// There properties are used for the inspector
+    /// </summary>
+    public Shape shape;
+    public Color color;
+
+    public float sphereRadius;
+
+    public Vector3 boxDimensions;
+
+    public Vector3 roundBoxDimensions;
+    public float roundBoxFactor;
+
+    public float torusOuterRadius;
+    public float torusInnerRadius;
+
+    public float coneHeight;
+    public Vector2 coneRatio;
+}
+
+/// IMPORTANT!
+/// GetSize function must be modified when adding/removing
+/// Properties to a shape
+/// IMPORTANT!
+public struct ShapeInfo
+{
+    public Vector3 position;
+    public int shape;
+    public Vector3 color;
+
+    public float sphereRadius;
+
+    public Vector3 boxDimensions;
+
+    public Vector3 roundBoxDimensions;
+    public float roundBoxFactor;
+
+    public float torusOuterRadius;
+    public float torusInnerRadius;
+
+    public float coneHeight;
+    public Vector2 coneRatio;
+
+    public static int GetSize()
+    {
+        return sizeof(float) * 19 + sizeof(int);
+    }
+}
